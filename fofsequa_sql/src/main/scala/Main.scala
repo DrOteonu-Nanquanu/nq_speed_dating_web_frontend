2787e77cfbc205257acd65f73938c70b0799b339
aeduin <aeduin@nanquanu.org>
2020-05-27 17:52:30 +0200

    worked on creating tables

--- fofsequa_sql/src/main/scala/Main.scala
+++ fofsequa_sql/src/main/scala/Main.scala
@@ -1,5 +1,5 @@
-class Main {
+object Main {
   def main(args: Array[String]): Unit = {
-    
+    println("Hello World!")
   }
 }
--- fofsequa_sql/src/main/scala/fofsequa_to_sql/fofsequa_to_sql.scala
+++ fofsequa_sql/src/main/scala/fofsequa_to_sql/fofsequa_to_sql.scala
@@ -1,5 +1,27 @@
 package fofsequa_to_sql
 
 object Fofsequa_to_sql {
+  def create_tables = Tables.Names.tables.map(
+    table_name =>
+      s"""CREATE TABLE $table_name
+        |
+        |""".stripMargin
+  )
+}
+
+object Tables {
+  object Names {
+    val field_of_interest = "field_of_interest"
+    val nq_project = "nq_project"
+    val project_foi_relation = "project_interesting_to"
+
+    val tables = List(
+      field_of_interest,
+      nq_project,
+      project_foi_relation,
+    )
+  }
 
 }
+
+case class Table()
