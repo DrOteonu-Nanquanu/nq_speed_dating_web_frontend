01f514d7ca9db4fee4a7aff4983d6a023aeb8d2b
aeduin <aeduin@nanquanu.org>
2020-07-05 12:36:46 +0200

    Conversion of Fofsequa to SQL now uses the reasoner instead of just syntax matching

--- fofsequa_sql/src/main/scala/fofsequa_to_sql/query_fofsequa.scala
+++ fofsequa_sql/src/main/scala/fofsequa_to_sql/query_fofsequa.scala
@@ -2,17 +2,18 @@ package fofsequa_to_sql
 
 import org.nanquanu.fofsequa._
 import org.nanquanu.fofsequa_reasoner._
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+
+import scala.util.{Failure, Success, Try}
 
 
 object Query_fofsequa {
-  def query(goal: String, kb: String): Option[_] = {
-    val answer = Main.evaluate_fofsequa(kb, goal) match {
-      case Some(ans) => ans
-      case None => return None
+  def query(goal: String, kb: String): Try[_] = {
+    val answer = FofsequaReasoner.evaluate_fofsequa(kb, goal) match {
+      case Success(ans) => ans
+      case Failure(exception) => return Failure(exception)
     }
 
-    answer match {
-
-    }
+    Failure(new NotImplementedException())
   }
 }
